289ed6cac72452e92a003b169f866edc3b6fcc65
ziazz78 <141522056+ziazz78@users.noreply.github.com>
2023-10-06 13:34:05 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Charts from "./Charts/index";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Charts />
+    </div>
+  );
+}
